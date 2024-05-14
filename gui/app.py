336eb53334de689686c302276a84ce989a26e522
frehml <frederic.hamelink@hotmail.com>
2024-05-14 12:43:52 +0200

    handle errors correctly

--- gui/app.py
+++ gui/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for, make_response
+from flask import Flask, render_template, redirect, request, make_response
 import requests
 from flasgger import Swagger
 
@@ -31,44 +31,38 @@ password = None
 
 session_data = dict()
 
-
 def save_to_session(key, value):
     session_data[key] = value
 
-
 def load_from_session(key):
-    return (
-        session_data.pop(key) if key in session_data else None
-    )  # Pop to ensure that it is only used once
-
+    return session_data.pop(key) if key in session_data else None  # Pop to ensure that it is only used once
 
 def succesful_request(r):
     return r.status_code == 200 or r.status_code == 201
 
-
-# ================================
-# FEATURE (list of public events)
-#
-# Retrieve the list of all public events. The webpage expects a list of (title, date, organizer) tuples.
-# Try to keep in mind failure of the underlying microservice
-# =================================
 @app.route("/")
 def home():
-    global username
-
+    global username, password
     if username is None:
         return render_template("login.html", username=username, password=password)
     else:
         params = {"is_public": True}
-        response = requests.get(f"{EVENTS_SERVICE_URL}/events/", params=params)
-
-        # if the request fails, return the home page with an empty list of events
-        if response.status_code != 200:
+        try:
+            response = requests.get(f"{EVENTS_SERVICE_URL}/events/", params=params)
+            # if the request fails, return the home page with an empty list of events
+            if response.status_code != 200:
+                return make_response(
+                    render_template(
+                        "home.html", username=username, password=password, events=[]
+                    ),
+                    response.status_code,
+                )
+        except:
             return make_response(
                 render_template(
                     "home.html", username=username, password=password, events=[]
                 ),
-                response.status_code,
+                400,
             )
 
         # Destructure the response to get the events
@@ -79,17 +73,14 @@ def home():
 
         return make_response(
             render_template(
-                "home.html", username=username, password=password, events=public_events
+                "home.html",
+                username=username,
+                password=password,
+                events=public_events
             ),
             response.status_code,
         )
 
-
-# ==========================
-# FEATURE (create an event)
-#
-# Given some data, create an event and send out the invites.
-# ==========================
 @app.route("/event", methods=["POST"])
 def create_event():
     """
@@ -124,6 +115,7 @@ def create_event():
         400:
             description: Event creation failed
     """
+    global username, password
     title, description, date, publicprivate, invites = (
         request.form["title"],
         request.form["description"],
@@ -132,64 +124,74 @@ def create_event():
         request.form["invites"],
     )
 
-    response = requests.post(
-        f"{EVENTS_SERVICE_URL}/events/",
-        json={
-            "date": date,
-            "organizer": username,
-            "title": title,
-            "description": description,
-            "is_public": publicprivate == "public",
-        },
-    )
-
-    if response.status_code != 201:
-        return make_response(response.content, response.status_code)
+    try:
+        response = requests.post(
+            f"{EVENTS_SERVICE_URL}/events/",
+            json={
+                "date": date,
+                "organizer": username,
+                "title": title,
+                "description": description,
+                "is_public": publicprivate == "public",
+            },
+        )
+        if response.status_code != 201:
+            return redirect("/")
+    except:
+        return redirect("/")
 
     event_id = response.json().get("event_id", None)
     invitees = invites.split(";")
 
     for invitee in invitees:
-        response = requests.post(
-            f"{INVITATIONS_SERVICE_URL}/invitations/",
-            json={"event_id": event_id, "invitee": invitee, "status": "Pending"},
-        )
-
-        if response.status_code != 201:
-            return make_response(response.content, response.status_code)
+        invitee = invitee.strip()
+        if invitee:
+            try:
+                response = requests.post(
+                    f"{INVITATIONS_SERVICE_URL}/invitations/",
+                    json={"event_id": event_id, "invitee": invitee, "status": "Pending"},
+                )
+                if response.status_code != 201:
+                    return redirect("/")
+            except:
+                return redirect("/")
 
     # Invite yourself and set status to Participate
     if username not in invitees:
-        response = requests.post(
-            f"{INVITATIONS_SERVICE_URL}/invitations/",
-            json={"event_id": event_id, "invitee": username, "status": "Participate"},
-        )
-
-    if response.status_code != 201:
-        return make_response(response.content, response.status_code)
-
+        try:
+            response = requests.post(
+                f"{INVITATIONS_SERVICE_URL}/invitations/",
+                json={"event_id": event_id, "invitee": username, "status": "Participate"},
+            )
+        finally:
+            return redirect("/")
+    
     return redirect("/")
 
-
-# ================================
-# FEATURE (calendar based on username)
-#
-# Retrieve the calendar of a certain user. The webpage expects a list of (id, title, date, organizer, status, Public/Private) tuples.
-# Try to keep in mind failure of the underlying microservice
-# =================================
 @app.route("/calendar", methods=["GET", "POST"])
 def calendar():
+    global username, password
     calendar_user = (
         request.form["calendar_user"] if "calendar_user" in request.form else username
     )
 
     if calendar_user is not username:
-        calendar_response = requests.get(
-            f"{CALENDARS_SERVICE_URL}/calendars/", params={"owner": calendar_user}
-        )
-        
-        # if the request fails, return the calendar page with an empty list of events
-        if calendar_response.status_code != 200:
+        try:
+            calendar_response = requests.get(
+                f"{CALENDARS_SERVICE_URL}/calendars/", params={"owner": calendar_user}
+            )
+
+            # if the request fails, return the calendar page with an empty list of events
+            if calendar_response.status_code != 200:
+                return render_template(
+                    "calendar.html",
+                    username=username,
+                    password=password,
+                    calendar_user=calendar_user,
+                    calendar=[],
+                    success=False,
+                )
+        except:
             return render_template(
                 "calendar.html",
                 username=username,
@@ -204,35 +206,43 @@ def calendar():
         success = True
 
     params = {"invitee": calendar_user, "status": "Participate"}
-    participating_events = requests.get(
-        f"{INVITATIONS_SERVICE_URL}/invitations/", params=params
-    )
-
-    if participating_events.status_code != 200:
+    try:
+        participating_events = requests.get(
+            f"{INVITATIONS_SERVICE_URL}/invitations/", params=params
+        )
+        if participating_events.status_code != 200:
+            my_invites = []
+        else:
+            my_invites = participating_events.json().get("invitations", [])
+    except:
         my_invites = []
-    else:
-        my_invites = participating_events.json().get("invitations", [])
 
     params = {"invitee": calendar_user, "status": "Maybe Participate"}
-    maybe_participating_events = requests.get(
-        f"{INVITATIONS_SERVICE_URL}/invitations/",
-        params=params,
-    )
 
-    if maybe_participating_events.status_code != 200:
+    try:
+        maybe_participating_events = requests.get(
+            f"{INVITATIONS_SERVICE_URL}/invitations/",
+            params=params,
+        )
+        if maybe_participating_events.status_code != 200:
+            my_invites += []
+        else:
+            my_invites += maybe_participating_events.json().get("invitations", [])
+    except:
         my_invites += []
-    else:
-        my_invites += maybe_participating_events.json().get("invitations", [])
 
     if success:
         calendar = []
         for invite in my_invites:
             event_id = invite["event_id"]
             params = {"id": event_id}
-            event_response = requests.get(
-                f"{EVENTS_SERVICE_URL}/events/",
-                params=params,
-            )
+            try:
+                event_response = requests.get(
+                    f"{EVENTS_SERVICE_URL}/events/",
+                    params=params,
+                )
+            except:
+                continue
 
             if event_response.status_code == 200:
                 event = event_response.json().get("events", [])[0]
@@ -258,67 +268,67 @@ def calendar():
         success=success,
     )
 
-
 @app.route("/share", methods=["GET"])
 def share_page():
+    global username, password
     return render_template(
         "share.html", username=username, password=password, success=None
     )
 
-
-# ========================================
-# FEATURE (share a calendar with a user)
-#
-# Share your calendar with a certain user. Return success = true / false depending on whether the sharing is succesful.
-# ========================================
 @app.route("/share", methods=["POST"])
 def share():
+    global username, password
     share_user = request.form["username"]
 
-    response = requests.put(
-        f"{CALENDARS_SERVICE_URL}/share",
-        json={"owner": username, "shared_with": share_user},
-    )
-
-    success = succesful_request(response)
+    try:
+        response = requests.put(
+            f"{CALENDARS_SERVICE_URL}/share",
+            json={"owner": username, "shared_with": share_user},
+        )
+        success = succesful_request(response)
+    except:
+        success = False
 
     return render_template(
         "share.html", username=username, password=password, success=success
     )
 
-
-# ================================
-# FEATURE (event details)
-#
-# Retrieve additional information for a certain event parameterized by an id. The webpage expects a (title, date, organizer, status, (invitee, participating)) tuples.
-# Try to keep in mind failure of the underlying microservice
-# =================================
 @app.route("/event/<eventid>")
 def view_event(eventid):
+    global username, password
     params = {"event": eventid}
-    invitations_response = requests.get(
-        f"{INVITATIONS_SERVICE_URL}/invitations/",
-        params=params,
-    )
 
-    if invitations_response.status_code != 200:
+    try:
+        invitations_response = requests.get(
+            f"{INVITATIONS_SERVICE_URL}/invitations/",
+            params=params,
+        )
+        if invitations_response.status_code != 200:
+            invitations = []
+        else:
+            invitations = invitations_response.json().get("invitations", [])
+    except:
         invitations = []
-    else:
-        invitations = invitations_response.json().get("invitations", [])
     
     success = username in [invite["invitee"] for invite in invitations]
 
     params = {"id": eventid}
-    event_response = requests.get(
-        f"{EVENTS_SERVICE_URL}/events/",
-        params=params,
-    )
 
-    if event_response.status_code != 200:
-        event = {}
-    else:
-        event = event_response.json().get("events", [])[0]
-    
+    try:
+        event_response = requests.get(
+            f"{EVENTS_SERVICE_URL}/events/",
+            params=params,
+        )
+        if event_response.status_code != 200:
+            return render_template(
+            "event.html", username=username, password=password, event={}, success=success
+        )
+        else:
+            event = event_response.json().get("events", [])[0]
+    except:
+        return render_template(
+            "event.html", username=username, password=password, event={}, success=success
+        )
     success = success or event["is_public"]
 
     if success:
@@ -336,7 +346,6 @@ def view_event(eventid):
     return render_template(
         "event.html", username=username, password=password, event=event, success=success
     )
-
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -360,19 +369,30 @@ def login():
         401:
             description: Login failed
     """
+    global username, password
     req_username, req_password = request.form["username"], request.form["password"]
 
-    response = requests.post(
-        f"{AUTH_SERVICE_URL}/login/",
-        json={"username": req_username, "password": req_password},
-    )
+    try:
+        response = requests.post(
+            f"{AUTH_SERVICE_URL}/login/",
+            json={"username": req_username, "password": req_password},
+        )
+        success = succesful_request(response)
+    except:
+        return make_response(
+            render_template(
+                "login.html",
+                username=username,
+                password=password,
+                success=False,
+                login=True,
+            ),
+            401,
+        )
 
-    success = succesful_request(response)
     save_to_session("success", success)
 
     if success:
-        global username, password
-
         username = req_username
         password = req_password
 
@@ -386,7 +406,6 @@ def login():
         ),
         response.status_code,
     )
-
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -410,19 +429,30 @@ def register():
         400:
             description: Registration failed
     """
+    global username, password
     req_username, req_password = request.form["username"], request.form["password"]
 
-    response = requests.post(
-        f"{AUTH_SERVICE_URL}/register/",
-        json={"username": req_username, "password": req_password},
-    )
+    try:
+        response = requests.post(
+            f"{AUTH_SERVICE_URL}/register/",
+            json={"username": req_username, "password": req_password},
+        )
+        success = succesful_request(response)
+    except:
+        return make_response(
+            render_template(
+                "login.html",
+                username=username,
+                password=password,
+                success=False,
+                registration=True,
+            ),
+            400,
+        )
 
-    success = succesful_request(response)
     save_to_session("success", success)
 
     if success:
-        global username, password
-
         username = req_username
         password = req_password
 
@@ -437,45 +467,46 @@ def register():
         response.status_code,
     )
 
-
-# ==============================
-# FEATURE (list invites)
-#
-# retrieve a list with all events you are invited to and have not yet responded to
-# ==============================
 @app.route("/invites", methods=["GET"])
 def invites():
+    global username, password
     params = {"invitee": username, "status": "Pending"}
-    response = requests.get(
-        f"{INVITATIONS_SERVICE_URL}/invitations/",
-        params=params,
-    )
 
-    if response.status_code != 200:
+    try:
+        response = requests.get(
+            f"{INVITATIONS_SERVICE_URL}/invitations/",
+            params=params,
+        )
+        if response.status_code != 200:
+            my_invites = []
+        else:
+            my_invites = response.json().get("invitations", [])
+    except:
         my_invites = []
-    else:
-        my_invites = response.json().get("invitations", [])
 
     invites = []
     for invite in my_invites:
         event_id = invite["event_id"]
         params = {"id": event_id}
-        event_response = requests.get(
-            f"{EVENTS_SERVICE_URL}/events/",
-            params=params,
-        )
-
-        if event_response.status_code == 200:
-            event = event_response.json().get("events", [])[0]
-            invites.append(
-                (
-                    event_id,
-                    event["title"],
-                    event["date"],
-                    event["organizer"],
-                    event["is_public"],
-                )
+        try:
+            event_response = requests.get(
+                f"{EVENTS_SERVICE_URL}/events/",
+                params=params,
             )
+
+            if event_response.status_code == 200:
+                event = event_response.json().get("events", [])[0]
+                invites.append(
+                    (
+                        event_id,
+                        event["title"],
+                        event["date"],
+                        event["organizer"],
+                        event["is_public"],
+                    )
+                )
+        except:
+            continue
 
     return make_response(
         render_template(
@@ -484,24 +515,21 @@ def invites():
         response.status_code,
     )
 
-
-# =======================
-# FEATURE (process invite)
-#
-# process an invite (accept, maybe, don't accept)
-# =======================
 @app.route("/invites", methods=["POST"])
 def process_invite():
+    global username, password
     eventId, status = request.json["event"], request.json["status"]
 
     params = {"invitee": username, "event": eventId, "status": status}
-    requests.patch(
-        f"{INVITATIONS_SERVICE_URL}/invitations/{eventId}/{username}",
-        params=params,
-    )
+    try:
+        requests.patch(
+            f"{INVITATIONS_SERVICE_URL}/invitations/{eventId}/{username}",
+            params=params,
+        )
+    except:
+        return redirect("/invites")
 
     return redirect("/invites")
-
 
 @app.route("/logout")
 def logout():
@@ -510,3 +538,6 @@ def logout():
     username = None
     password = None
     return redirect("/")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
